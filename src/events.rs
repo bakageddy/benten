d8ccc9a7050efdc777250c27630248b1062fc3ef
Dinesh Kumar G <gdineshda@gmail.com>
2023-12-19 01:16:54 +0530

    Add: Cursor functionality

--- src/events.rs
+++ src/events.rs
@@ -1,20 +1,61 @@
 use std::time::Duration;
 
-use crossterm::event::{self, Event, KeyEventKind, KeyCode, KeyModifiers};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 
 use crate::app::App;
 
 pub fn handle_events(app: &mut App) -> anyhow::Result<()> {
     if event::poll(Duration::from_millis(16)).unwrap_or(false) {
-        if let Event::Key(e) = event::read()? {
-            if e.kind == KeyEventKind::Press {
-                match e.code {
-                    KeyCode::Char('c') if e.modifiers == KeyModifiers::CONTROL => {
-                        app.quit();
-                    },
-                    _ => {},
-                }
-            }
+        let e = event::read()?;
+
+        match e {
+            Event::Key(KeyEvent {
+                kind: KeyEventKind::Press,
+                code: KeyCode::Char(c),
+                modifiers: KeyModifiers::NONE | KeyModifiers::SHIFT,
+                ..
+            }) => app.insert_char(c),
+
+            Event::Key(KeyEvent {
+                kind: KeyEventKind::Press,
+                code: KeyCode::Char('c'),
+                modifiers: KeyModifiers::CONTROL,
+                ..
+            }) => app.quit(),
+
+            Event::Key(KeyEvent {
+                code: KeyCode::Esc, ..
+            }) => app.quit(),
+
+            Event::Key(KeyEvent {
+                code: KeyCode::Enter,
+                modifiers: KeyModifiers::NONE,
+                kind: KeyEventKind::Press,
+                ..
+            }) => app.get_manga_list(),
+
+            Event::Key(KeyEvent {
+                code: KeyCode::Left,
+                kind: KeyEventKind::Press,
+                modifiers: KeyModifiers::NONE,
+                ..
+            }) => app.move_cursor_left(),
+
+            Event::Key(KeyEvent {
+                code: KeyCode::Right,
+                kind: KeyEventKind::Press,
+                modifiers: KeyModifiers::NONE,
+                ..
+            }) => app.move_cursor_right(),
+
+            Event::Key(KeyEvent {
+                code: KeyCode::Backspace,
+                kind: KeyEventKind::Press,
+                modifiers: KeyModifiers::NONE,
+                ..
+            }) => app.delete_current_char(),
+
+            _ => {}
         }
     }
     Ok(())
